eed2f3b9d21cbc974eef1782034a42489e2744dd
xiaobo.song <xiaobo.song@sinocare.com>
2025-10-02 14:25:09 +0800

    feat(process): 添加第三步和第四步的子菜单及页面结构

--- pages/process/three.vue
+++ pages/process/three.vue
@@ -1,7 +1,34 @@
 <template>
-  <div>第三步</div>
+  <div class="flex h-full w-full bg-white rounded-[4px] process-page-container">
+    <SubMenuSidebar :active-tab="isActiveTab" @on-change="tabChange" />
+    <div class="flex-1 mx-[20px] my-[20px]">
+      <NuxtPage />
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SubMenuSidebar from '@/components/sub-mune-sidebar/index.vue'
+const router = useRouter()
+const route = useRoute()
+console.log('route', route)
 
-<style lang="less" scoped></style>
+const isActiveTab = ref(1)
+
+const tabChange = (tab: number) => {
+  isActiveTab.value = tab
+  if (tab === 1) {
+    router.push('/process/three/file')
+  } else if (tab === 2) {
+    router.push('/process/three/form')
+  } else if (tab === 3) {
+    router.push('/process/three/render')
+  }
+}
+
+onMounted(() => {
+  if (route.path === '/process/three') {
+    router.replace('/process/three/file')
+  }
+})
+</script>
