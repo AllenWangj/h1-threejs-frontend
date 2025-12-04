929fac7388b7ac72cba261cbba8673b9e9e1a59c
WIN-1ETVCJSERNV\Administrator <805822426@qq.com>
2025-12-05 07:32:42 +0800

    保存代码

---
+++ pages/ifc/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div style="width:800px;height:800px" ref="three">222</div>
+</template>
+<script setup lang="ts">
+import {ref,onMounted} from "vue"
+import {useRender} from "./source"
+const three = ref()
+onMounted(() =>{
+    const {IFC} = useRender()
+    new IFC(three.value)
+})
+</script>
